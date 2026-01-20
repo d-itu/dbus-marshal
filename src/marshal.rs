370e40402d36943c7662d6c07506375bb63f1d3c
kamiduki <kamiduki_0@outlook.com>
2026-01-20 23:00:14 +0800

    make marshal argument passed by value

--- src/marshal.rs
+++ src/marshal.rs
@@ -35,13 +35,7 @@ pub unsafe trait Write: Copy {
     fn write_byte(&mut self, byte: u8) -> Result<()>;
 
     #[must_use]
-    fn write_value<T: Marshal + ?Sized>(&mut self, v: &T) -> Result<()> {
-        v.marshal(self)?;
-        Ok(())
-    }
-
-    #[must_use]
-    fn write_primitive<T: Marshal>(&mut self, v: T) -> Result<()> {
+    fn write_value<T: Marshal>(&mut self, v: T) -> Result<()> {
         v.marshal(self)?;
         Ok(())
     }
@@ -113,11 +107,11 @@ unsafe impl Write for Span {
     }
 }
 
-pub trait Marshal {
+pub trait Marshal: Copy {
     type Signature: Signature + ?Sized;
 
     #[must_use]
-    fn marshal<W: Write>(&self, w: &mut W) -> Result<()>;
+    fn marshal<W: Write>(self, w: &mut W) -> Result<()>;
 }
 
 macro_rules! impl_marshal {
@@ -125,7 +119,7 @@ macro_rules! impl_marshal {
         $(impl Marshal for $t {
             type Signature = $t;
 
-            fn marshal<W: Write>(&self, w: &mut W) -> Result<()> {
+            fn marshal<W: Write>(self, w: &mut W) -> Result<()> {
                 w.align_to(core::mem::align_of::<$t>())?;
                 w.write_bytes(&self.to_ne_bytes())?;
                 Ok(())
@@ -134,12 +128,20 @@ macro_rules! impl_marshal {
     };
 }
 
+impl<T: Marshal> Marshal for &T {
+    type Signature = T::Signature;
+
+    fn marshal<W: Write>(self, w: &mut W) -> Result<()> {
+        w.write_value(self)
+    }
+}
+
 impl_marshal!(u8, i16, u16, i32, u32, i64, u64, f64);
 
 impl Marshal for bool {
     type Signature = bool;
 
-    fn marshal<W: Write>(&self, w: &mut W) -> Result<()> {
+    fn marshal<W: Write>(self, w: &mut W) -> Result<()> {
         w.align_to(4)?;
         match self {
             true => 1u32,
@@ -151,88 +153,97 @@ impl Marshal for bool {
 }
 
 fn write_string<W: Write>(w: &mut W, string: &[u8]) -> Result<()> {
-    let len = string.len() as u32;
-    w.write_primitive(len)?;
+    w.write_value(string.len() as u32)?;
     w.write_bytes(string)?;
     w.write_byte(0)
 }
 
-impl Marshal for strings::String {
+impl Marshal for &strings::String {
     type Signature = strings::String;
 
-    fn marshal<W: Write>(&self, w: &mut W) -> Result<()> {
+    fn marshal<W: Write>(self, w: &mut W) -> Result<()> {
         write_string(w, self)
     }
 }
 
-impl Marshal for strings::Signature {
+impl Marshal for &strings::Signature {
     type Signature = strings::Signature;
 
-    fn marshal<W: Write>(&self, w: &mut W) -> Result<()> {
+    fn marshal<W: Write>(self, w: &mut W) -> Result<()> {
         w.write_byte(self.len() as _)?;
         w.write_bytes(self)?;
         w.write_byte(0)
     }
 }
 
-impl Marshal for strings::ObjectPath {
+impl Marshal for &strings::ObjectPath {
     type Signature = strings::ObjectPath;
 
-    fn marshal<W: Write>(&self, w: &mut W) -> Result<()> {
+    fn marshal<W: Write>(self, w: &mut W) -> Result<()> {
         write_string(w, self)
     }
 }
 
+#[derive(Clone, Copy)]
 pub struct Variant<T: Marshal>(T);
 
 impl<T: Marshal> Marshal for Variant<T> {
     type Signature = strings::ObjectPath;
 
-    fn marshal<W: Write>(&self, w: &mut W) -> Result<()> {
+    fn marshal<W: Write>(self, w: &mut W) -> Result<()> {
         let sig = crate::signature_bytes!(Self::Signature);
         let sig = strings::Signature::from_bytes(sig);
         w.write_value(sig)?;
-        w.write_value(&self.0)
+        w.write_value(self.0)
     }
 }
 
-pub struct Array<T: Marshal, I>(pub I)
+#[derive(Clone, Copy)]
+pub struct Array<T, I>(pub I)
 where
+    I: Copy,
+    T: Marshal,
     for<'a> &'a I: IntoIterator<Item = &'a T>;
 
-impl<T: Marshal, I> Marshal for Array<T, I>
+impl<T, I> Marshal for &Array<T, I>
 where
+    I: Copy,
+    T: Marshal,
     for<'a> &'a I: IntoIterator<Item = &'a T>,
 {
     type Signature = signature::Array<T::Signature>;
 
-    fn marshal<W: Write>(&self, w: &mut W) -> Result<()> {
+    fn marshal<W: Write>(self, w: &mut W) -> Result<()> {
         let mut len_writer = w.skip_aligned(4)?;
         for v in &self.0 {
             w.write_value(v)?;
         }
         let len = w.position() - len_writer.position();
-        len_writer.write_primitive(len as u32)
+        len_writer.write_value(len as u32)
     }
 }
 
-pub struct DictEntry<'k, 'v, K: Marshal + ?Sized, V: Marshal + ?Sized>(pub &'k K, pub &'v V);
-impl<'k, 'v, K: Marshal + ?Sized, V: Marshal + ?Sized> Marshal for DictEntry<'k, 'v, K, V> {
+#[derive(Clone, Copy)]
+pub struct DictEntry<K: Marshal, V: Marshal>(pub K, pub V);
+impl<K: Marshal, V: Marshal> Marshal for DictEntry<K, V> {
     type Signature = signature::DictEntry<K::Signature, V::Signature>;
 
-    fn marshal<W: Write>(&self, w: &mut W) -> Result<()> {
+    fn marshal<W: Write>(self, w: &mut W) -> Result<()> {
         w.align_to(8)?;
         w.write_value(self.0)?;
         w.write_value(self.1)
     }
 }
 
-// pub fn marshal<Value: Marshal + ?Sized>(value: &Value) {}
+pub fn calc_size<Value: Marshal>(value: Value) {
+    let mut count = 0;
+    value.marshal(&mut count).unwrap();
+}
 
 #[test]
 fn test_marshal() {
-    let arr = [0];
-    Array(arr).marshal(&mut 0).unwrap();
-    let dict = Array([DictEntry(&0, &1)]);
-    dict.marshal(&mut 0).unwrap();
+    0.marshal(&mut 0).unwrap();
+    DictEntry(0, &&1).marshal(&mut 0).unwrap();
+    let arr = Array([0]);
+    arr.marshal(&mut 0).unwrap();
 }
